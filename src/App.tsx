7792b8cd7ad724252ebd90a4bf804f72f22216b9
AnferneeDev <anfernee.developer@gmail.com>
2026-01-14 18:11:41 -0400

    feat: add shadcn and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useCallback, useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const triple = useCallback(() => count * 3, [count]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <React.Fragment>
+      <div className="flex flex-col items-center justify-center bg-slate-100 h-screen">
+        <h1>hello</h1>
+        <Button onClick={() => setCount(count + 1)}>Click me</Button>
+        <p>Count: {count}</p>
+        <p>Triple: {triple()}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
